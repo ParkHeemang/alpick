58993531f5cbebc84606813e3025734e1372b2ef
aa <aa@DESKTOP-OCIGRN5>
2018-01-06 17:19:03 +0900

    Analysis class파일 업데이트

--- alpick/src/com/Control/Analysis.java
+++ alpick/src/com/Control/Analysis.java
@@ -1,12 +1,61 @@
 package com.Control;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Analysis {
 	
 	StringBuffer cmd = null;
+	StringBuffer output = null;
+	String url = "python C:\\Users\\aa\\Desktop\\PSFinal\\DataAnalysis";
+	String filename = "test.py";
 	
 	public void analysis(String id, String user_type) {
 		
-		String dirPath = System.getProperty("");
+		cmd = new StringBuffer();
+		output = new StringBuffer();
+		
+		cmd.append(url);
+		cmd.append("\\");
+		cmd.append(filename);
+		cmd.append(" ");
+		cmd.append(id);
+		cmd.append(" ");
+		cmd.append(user_type);
+		
+		try {
+			
+			Runtime r = Runtime.getRuntime();
+			Process p = r.exec(cmd.toString());
+			
+			int resultSign = -1;
+			
+			try {
+				resultSign = p.waitFor();
+			} catch(Exception e) {
+				System.out.println(e);
+			}
+			
+			if(resultSign == 0) {
+				
+				BufferedReader bfr = new BufferedReader(new InputStreamReader(p.getInputStream()));
+				
+				String line = "";
+				while((line = bfr.readLine()) != null) {
+					output.append(line);
+					output.append("\n");
+				}
+				
+				bfr.close();
+				
+			} else {
+				System.out.println(resultSign);
+			}
+			
+		} catch(Exception e) {
+			System.out.println(e);
+		}
+		
 	}
 
 }
